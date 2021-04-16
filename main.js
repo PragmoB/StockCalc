cc944f86c6811b37646427ba709206fd4e103c2e
PragmoB <peullaegeumo@gmail.com>
2021-04-17 01:34:44 +0900

    vue.js release

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-$(document).ready(function() {
+var handle = new Vue({
+    el: '#wrap',
+    data: {
+        capital : 0,
+        earning : 0,
+        ROE : 0,
+        required : 0,
+        over_earning_ratio : 0,
+        over_earning : 0,
+        premium : 0,
+        ideal_market_cap : 0,
+        stocks : 0,
+        ideal_price : 0,
 
-    $("#calculate").click(function() {
-        
-        var ROE = $("#earning").val() / $("#capital").val() * 100;
-        var OverEarningRatio = ROE - $("#required").val();
-        var OverEarning = $("#capital").val() * 100000000 *  OverEarningRatio / 100;
-        var premium = OverEarning / $("#required").val() * 100;
-        var IdealMarketCap = premium + $("#capital").val() * 100000000;
-        var IdealPrice = IdealMarketCap / $("#stocks").val();
+        calc_done : false
+    },
+    methods: {
+        calculate: function() {
+            this.calc_done = false;
 
-        $("#ROE").text(ROE.toFixed(2));
-        $("#over-earning-ratio").text(OverEarningRatio.toFixed(2));
-        $("#over-earning").text(numberWithCommas(OverEarning.toFixed(0)));
-        $("#premium").text(numberWithCommas(premium.toFixed(0)));
-        $("#ideal-market-cap").text(numberWithCommas(IdealMarketCap.toFixed(0)));
-        $("#ideal-price").text(numberWithCommas(IdealPrice.toFixed(0)));
+            this.ROE = this.earning / this.capital * 100;
+            this.over_earning_ratio = this.ROE - this.required;
+            this.over_earning = this.capital * 100000000 * this.over_earning_ratio / 100;
+            this.premium = this.over_earning / this.required * 100;
+            this.ideal_market_cap = this.premium + this.capital * 100000000;
+            this.ideal_price = this.ideal_market_cap / this.stocks;
 
-        $("#result").css("display", "block");
-    });
-    
+            this.ROE = numberWithCommas(Math.round(this.ROE * 100) / 100);
+            this.over_earning_ratio = numberWithCommas(Math.round(this.over_earning_ratio * 100) / 100);
+            this.over_earning = numberWithCommas(Math.round(this.over_earning));
+            this.premium = numberWithCommas(Math.round(this.premium));
+            this.ideal_market_cap = numberWithCommas(Math.round(this.ideal_market_cap));
+            this.ideal_price = numberWithCommas(Math.round(this.ideal_price));
+
+            this.calc_done = true;
+        }
+    }
 });
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
-
